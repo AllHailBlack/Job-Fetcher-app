dc0844b21213cda144a631b8c39624e59088820e
AllHailBlack <lakshaykumar96.lk@gmail.com>
2025-11-26 07:51:36 +0000

    Changes

--- app.js
+++ app.js
@@ -48,7 +48,8 @@ const JOB_API_URL = 'https://remotive.com/api/remote-jobs?search='; // example p
 /* =========================
    EXPRESS + MIDDLEWARE
    ========================= */
-const app = express();
+app.use(express.static('public'));
+   const app = express();
 app.use(cors());
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
